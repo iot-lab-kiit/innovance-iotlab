3ee6b1497a4d35a35e41cd9606c0e3b78bf3aeb3
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2023-11-11 23:41:21 +0530

    Api integration done

--- page-components/Home/Venue.jsx
+++ page-components/Home/Venue.jsx
@@ -1,121 +1,170 @@
-import {Container, Grid, Stack} from "@mui/material";
+import { Container, Grid, Stack } from "@mui/material";
 import Box from "@mui/material/Box";
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 import Button from "@mui/material/Button";
 // import Venue1 from "../../public/backgrounds/venue1.JPG";
 // import Venue2 from "../../public/backgrounds/venue2.JPG";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
+export default function Index({ venueRef }) {
+  const [img, setImg] = useState(true);
+  const [time, setTime] = useState(5);
+  useEffect(() => {
+    if (time > 0) {
+      setTimeout(function () {
+        setTime(time - 1);
+      }, 1000);
+    }
+    if (time === 0) {
+      setImg(!img);
+      setTime(5);
+    }
+  }, [img, time]);
 
-export default function Index({venueRef}) {
-
-    const [img, setImg] = useState(true)
-    const [time, setTime] = useState(5);
-    useEffect(() => {
-        if (time > 0) {
-            setTimeout(function () {
-                setTime(time - 1);
-            }, 1000);
-        }
-        if (time === 0){
-            setImg(!img);
-            setTime(5)
-        }
-    }, [img, time]);
-
-    return (
-        <>
+  return (
+    <>
+      <Box
+        ref={venueRef}
+        width={"100%"}
+        sx={{
+          background: img
+            ? `url('https://i.imgur.com/TaBfY45.jpg')`
+            : `url('https://i.imgur.com/oKOCkRA.jpeg')`,
+          //   background: `url('https://i.imgur.com/TaBfY45.jpg')`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          height: { xl: "750px", lg: "630px", xs: "500px" },
+          backgroundColor: "rgba(0, 0, 0, 0.7)",
+        }}
+      >
+        <Box
+          width={"100%"}
+          sx={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}
+          height={"100%"}
+          display={"flex"}
+          alignItems={"center"}
+        >
+          <Container maxWidth={"lg"}>
             <Box
-                ref={venueRef}
-                width={'100%'}
-                sx={{
-                    // background: img ? `url('https://i.imgur.com/TaBfY45.jpg')` : `url('https://i.imgur.com/bzY3nBD.jpg')`,
-                    background:`url('https://i.imgur.com/TaBfY45.jpg')`,
-                    backgroundSize: 'cover',
-                    backgroundRepeat: 'no-repeat',
-                    height: {xl: '750px', lg: '630px', xs: '500px'},
-                    backgroundColor: 'rgba(0, 0, 0, 0.7)'
-                }}
+              width={"100%"}
+              display={"flex"}
+              justifyContent={"center"}
+              flexDirection={"column"}
+              alignItems={"center"}
+              mt={4}
             >
-                <Box width={'100%'} sx={{backgroundColor: 'rgba(0, 0, 0, 0.6)'}} height={'100%'} display={'flex'} alignItems={'center'}>
-                    <Container maxWidth={'lg'}>
-                        <Box width={'100%'} display={'flex'} justifyContent={'center'} flexDirection={'column'} alignItems={'center'} mt={4}>
-                            <Box color={'#FFF'} fontSize={{md: '47px', xs: '35px'}} width={'100%'} display={'flex'} justifyContent={'center'} alignItems={'center'}>
-                                <Box>
-                                    The Event
-                                </Box>
-                                <Box ml={{md:1.5, xs: 1}} fontWeight={600}>
-                                    Venue
-                                </Box>
-                            </Box>
-                            <Box width={'100%'} display={'flex'} justifyContent={'center'} alignItems={'center'} color={'#c3c3c3'} mt={{md:3, xs: 2}} mb={{md:4, xs: 3}}>
-                                <Box borderBottom={'2px solid #c3c3c3'} height={'2px'} width={{md: '100px', xs: '130px'}} sx={{opacity: 0.5}}/>
-                                <LocationOnIcon sx={{ml: 3, mr: 3}} />
-                                <Box borderBottom={'2px solid #c3c3c3'} height={'2px'} width={{md: '100px', xs: '130px'}} sx={{opacity: 0.5}}/>
-                            </Box>
-                            <Box textAlign={'center'} color={'#bcbcbc'} maxWidth={'700px'}>
-                                Ahoy KIITians! Join us for Innovance in campus 17! Mark your calendars from 4th November to 5th November and be ready for unlimited fun!                            </Box>
-                            <Box display={'flex'} justifyContent={'center'} alignItems={'center'} width={'100%'} mt={10}>
-                                {/* {
-                                    ['Campus 15', 'Campus 17'].map((each, index) => (
-                                        <Button
-                                            sx={{
-                                                ml: index ? 4 : 0,
-                                                borderRadius: '30px',
-                                                textTransform: 'none',
-                                                py: 0.3,
-                                                px: 4,
-                                                fontSize: {md: '20px', xs: '16px'},
-                                                border: '1.5px solid #FFF',
-                                                "&:hover": {
-                                                    // background: "#4763B7",
-                                                    // border: '1.5px solid #4763B7'
-                                                    border: '1.5px solid #FFF'
-                                                },
-                                            }}
-                                            onClick={async () => {
-                                                window.open(index ? 'https://goo.gl/maps/Szg68CUGrzvrLKED7' : 'https://maps.app.goo.gl/H3NECFxgJbkM3Uj77?g_st=ic', 'blank')
-                                            }}
-                                            variant={'outlined'}
-                                            color={'white'}
-                                            disableElevation
-                                            key={index}
-                                        >
-                                            {each}
-                                        </Button>
-                                    ))
-                                } */}
-                                <Button
-                                            sx={{
-                                                ml:0,
-                                                borderRadius: '30px',
-                                                textTransform: 'none',
-                                                py: 0.3,
-                                                px: 4,
-                                                fontSize: {md: '20px', xs: '16px'},
-                                                border: '1.5px solid #FFF',
-                                                "&:hover": {
-                                                    // background: "#4763B7",
-                                                    // border: '1.5px solid #4763B7'
-                                                    border: '1.5px solid #FFF'
-                                                },
-                                            }}
-                                            onClick={async () => {
-                                                window.open('https://goo.gl/maps/Szg68CUGrzvrLKED7', 'blank')
-                                            }}
-                                            variant={'outlined'}
-                                            color={'white'}
-                                            disableElevation
-                                        >
-                                            Campus 17
-                                        </Button>
-                            </Box>
-                        </Box>
-
-                    </Container>
+              <Box
+                color={"#FFF"}
+                fontSize={{ md: "47px", xs: "35px" }}
+                width={"100%"}
+                display={"flex"}
+                justifyContent={"center"}
+                alignItems={"center"}
+              >
+                <Box>The Event</Box>
+                <Box ml={{ md: 1.5, xs: 1 }} fontWeight={600}>
+                  Venue
                 </Box>
-
+              </Box>
+              <Box
+                width={"100%"}
+                display={"flex"}
+                justifyContent={"center"}
+                alignItems={"center"}
+                color={"#c3c3c3"}
+                mt={{ md: 3, xs: 2 }}
+                mb={{ md: 4, xs: 3 }}
+              >
+                <Box
+                  borderBottom={"2px solid #c3c3c3"}
+                  height={"2px"}
+                  width={{ md: "100px", xs: "130px" }}
+                  sx={{ opacity: 0.5 }}
+                />
+                <LocationOnIcon sx={{ ml: 3, mr: 3 }} />
+                <Box
+                  borderBottom={"2px solid #c3c3c3"}
+                  height={"2px"}
+                  width={{ md: "100px", xs: "130px" }}
+                  sx={{ opacity: 0.5 }}
+                />
+              </Box>
+              <Box textAlign={"center"} color={"#bcbcbc"} maxWidth={"700px"}>
+                Ahoy KIITians! Join us for Innovance in campus 7 and 17! Mark
+                your calendars from 18th November to 19th November and be ready
+                for unlimited fun!{" "}
+              </Box>
+              <Box
+                display={"flex"}
+                justifyContent={"center"}
+                alignItems={"center"}
+                width={"100%"}
+                mt={10}
+              >
+                {["Campus 7", "Campus 17"].map((each, index) => (
+                  <Button
+                    sx={{
+                      ml: index ? 4 : 0,
+                      borderRadius: "30px",
+                      textTransform: "none",
+                      py: 0.3,
+                      px: 4,
+                      fontSize: { md: "20px", xs: "16px" },
+                      border: "1.5px solid #FFF",
+                      "&:hover": {
+                        // background: "#4763B7",
+                        // border: '1.5px solid #4763B7'
+                        border: "1.5px solid #FFF",
+                      },
+                    }}
+                    onClick={async () => {
+                      window.open(
+                        index
+                          ? "https://maps.app.goo.gl/LX9dVmxx6YWW4Lev7"
+                          : "https://maps.app.goo.gl/KkV3Y8JSegmqX47x7",
+                        "blank"
+                      );
+                    }}
+                    variant={"outlined"}
+                    color={"white"}
+                    disableElevation
+                    key={index}
+                  >
+                    {each}
+                  </Button>
+                ))}
+                {/* <Button
+                  sx={{
+                    ml: 0,
+                    borderRadius: "30px",
+                    textTransform: "none",
+                    py: 0.3,
+                    px: 4,
+                    fontSize: { md: "20px", xs: "16px" },
+                    border: "1.5px solid #FFF",
+                    "&:hover": {
+                      // background: "#4763B7",
+                      // border: '1.5px solid #4763B7'
+                      border: "1.5px solid #FFF",
+                    },
+                  }}
+                  onClick={async () => {
+                    window.open(
+                      "https://goo.gl/maps/Szg68CUGrzvrLKED7",
+                      "blank"
+                    );
+                  }}
+                  variant={"outlined"}
+                  color={"white"}
+                  disableElevation
+                >
+                  Campus 17
+                </Button> */}
+              </Box>
             </Box>
-        </>
-    )
+          </Container>
+        </Box>
+      </Box>
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import Navbar from "../page-components/Home/Navbar";
 import Box from "@mui/material/Box";
 import Main from "../page-components/Home/Main";
@@ -9,58 +9,58 @@ import Sponsors from "../page-components/Home/Sponsors";
 import Gallery from "../page-components/Home/Gallery";
 import Contact from "../page-components/Home/Contact";
 import Venue from "../page-components/Home/Venue";
-import {useRef} from 'react';
+import { useRef } from "react";
 
 export default function Index() {
+  const mainRef = useRef(null);
+  const aboutRef = useRef(null);
+  const scheduleRef = useRef(null);
+  const venueRef = useRef(null);
+  const planRef = useRef(null);
+  const sponsorRef = useRef(null);
+  const galleryRef = useRef(null);
+  const contactRef = useRef(null);
 
-    const mainRef = useRef(null);
-    const aboutRef = useRef(null);
-    const scheduleRef = useRef(null);
-    const venueRef = useRef(null);
-    const planRef = useRef(null);
-    const sponsorRef = useRef(null);
-    const galleryRef = useRef(null);
-    const contactRef = useRef(null);
+  // console.log("hii")
 
-    console.log("hii")
-
-    return (
-        <>
-            <ThemeProvider theme={createTheme({
-
-                palette: {
-                    primary: {
-                        main: '#4763B7',
-                    },
-                    secondary: {
-                        main: '#204341',
-                    },
-                    white: {
-                        main: '#FFF',
-                    },
-                },
-            })}>
-                <Box color={'#000'} bgcolor={'#FFF'}>
-                    <Navbar
-                        mainRef={mainRef}
-                        aboutRef={aboutRef}
-                        scheduleRef={scheduleRef}
-                        venueRef={venueRef}
-                        planRef={planRef}
-                        sponsorRef={sponsorRef}
-                        galleryRef={galleryRef}
-                        contactRef={contactRef}
-                    />
-                    <Main mainRef={mainRef} scheduleRef={scheduleRef} />
-                    <About aboutRef={aboutRef} />
-                    {/* <Schedule scheduleRef={scheduleRef} /> */}
-                    <Venue venueRef={venueRef} />
-                    {/*<Plans planRef={planRef} />*/}
-                    <Sponsors sponsorRef={sponsorRef} />
-                    <Gallery galleryRef={galleryRef} />
-                    <Contact contactRef={contactRef} />
-                </Box>
-            </ThemeProvider>
-        </>
-    )
+  return (
+    <>
+      <ThemeProvider
+        theme={createTheme({
+          palette: {
+            primary: {
+              main: "#4763B7",
+            },
+            secondary: {
+              main: "#204341",
+            },
+            white: {
+              main: "#FFF",
+            },
+          },
+        })}
+      >
+        <Box color={"#000"} bgcolor={"#FFF"}>
+          <Navbar
+            mainRef={mainRef}
+            aboutRef={aboutRef}
+            scheduleRef={scheduleRef}
+            venueRef={venueRef}
+            planRef={planRef}
+            sponsorRef={sponsorRef}
+            galleryRef={galleryRef}
+            contactRef={contactRef}
+          />
+          <Main mainRef={mainRef} scheduleRef={scheduleRef} />
+          <About aboutRef={aboutRef} />
+          {/* <Schedule scheduleRef={scheduleRef} /> */}
+          <Venue venueRef={venueRef} />
+          {/*<Plans planRef={planRef} />*/}
+          <Sponsors sponsorRef={sponsorRef} />
+          <Gallery galleryRef={galleryRef} />
+          <Contact contactRef={contactRef} />
+        </Box>
+      </ThemeProvider>
+    </>
+  );
 }
