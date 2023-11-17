d1c67905ad53077c4ad2fb0d7bc48dd9c129b108
anshulkumarx7 <anshulkumarx7@gmail.com>
2023-11-18 02:23:02 +0530

    added closed message

--- components/registrationPaused.js
+++ components/registrationPaused.js
@@ -35,10 +35,10 @@ export default function Registerationpaused() {
                 <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
                     <Stack align={"center"}>
                         <Heading fontSize={"4xl"} textAlign={"center"}>
-                            Registerations Paused
+                            Registerations Closed 😊
                         </Heading>
                         <Heading fontSize={"2xl"} textAlign={"center"}>
-                        We are delighted by the overwhelming responses, and we're excited to announce that registration will be opening soon for another round of participation!
+                        Thank you for the overwhelming response to our event; registrations are now closed. We look forward to welcoming you to our next exciting event in the future!
                         </Heading>
                         {/* <Typography variant="body1">We are pleased with the overwhemling responses! See you in the event</Typography> */}
                     </Stack>
